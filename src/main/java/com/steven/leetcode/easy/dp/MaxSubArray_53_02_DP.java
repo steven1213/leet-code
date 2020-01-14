f2c795cb4434d37b312730be06ae8596c7e5b6f5
steven <caolei_cd@keruyun.com>
2020-01-14 17:28:45 +0800

    最大子序和-leetcode-53-DP

---
+++ src/main/java/com/steven/leetcode/easy/dp/MaxSubArray_53_02_DP.java
@@ -0,0 +1,29 @@
+package com.steven.leetcode.easy.dp;
+
+/**
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），
+ * 返回其最大和。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [-2,1,-3,4,-1,2,1,-5,4],
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximum-subarray
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MaxSubArray_53_02_DP {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length, maxSum = nums[0];
+        for (int i = 1; i < n; ++i) {
+            if (nums[i - 1] > 0) {
+                nums[i] += nums[i - 1];
+            }
+            maxSum = Math.max(nums[i], maxSum);
+        }
+        return maxSum;
+    }
+
+}
